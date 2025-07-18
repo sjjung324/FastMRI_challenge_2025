c62e631ed5724aa4afefb1f9d8b4ba732fc92e1f
jaehyuk4175 <mgahn0706@snu.ac.kr>
2025-07-18 21:16:05 +0900

    Feature VarNet 추가

--- utils/learning/train_part.py
+++ utils/learning/train_part.py
@@ -10,7 +10,8 @@ from collections import defaultdict
 from utils.data.load_data import create_data_loaders
 from utils.common.utils import save_reconstructions, ssim_loss
 from utils.common.loss_function import SSIMLoss
-from utils.model.varnet import VarNet
+# from utils.model.varnet import VarNet
+from utils.model.feature_varnet import FeatureVarNet_sh_w as VarNet
 
 import os
 
