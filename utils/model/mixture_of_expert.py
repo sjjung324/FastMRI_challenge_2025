acea0336b6d52c29d182a9e0c1921ffe1ad704bf
jaehyuk4175 <mgahn0706@snu.ac.kr>
2025-07-28 22:49:30 +0900

    acc MoE 기능 추가

--- utils/model/mixture_of_expert.py
+++ utils/model/mixture_of_expert.py
@@ -1,21 +1,31 @@
 import torch
 import torch.nn as nn
-from feature_varnet import FeatureVarNet_sh_w as VarNet
+from utils.model.feature_varnet import FeatureVarNet_sh_w as VarNet
 from simple_classifier import SimpleClassifier
+from acc_classifier import acc_classifier
 
 class MoEModel(nn.Module):
-    def __init__(self, brain_model_args, knee_model_args):
+    def __init__(self, brain_model_acc4_args, brain_model_acc8_args, knee_model_acc4_args, knee_model_acc8_args):
         super().__init__()
-        self.brain_net = VarNet(**brain_model_args)
-        self.knee_net  = VarNet(**knee_model_args)
+        self.brain_net_acc4 = VarNet(**brain_model_acc4_args)
+        self.brain_net_acc8 = VarNet(**brain_model_acc8_args)
+        self.knee_net_acc4 = VarNet(**knee_model_acc4_args)
+        self.knee_net_acc8 = VarNet(**knee_model_acc8_args)
         self.classifier = SimpleClassifier()
+        self.acc_classifier = acc_classifier()
 
-    def load_models(self, brain_model_path, knee_model_path, classifier_path, device):
-        brain_ckpt = torch.load(brain_model_path, map_location=device, weights_only=False)
-        self.brain_net.load_state_dict(brain_ckpt['model'])
+    def load_models(self, brain_model_acc4_path, brain_model_acc8_path, knee_model_acc4_path, knee_model_acc8_path, classifier_path, device):
+        brain_ckpt = torch.load(brain_model_acc4_path, map_location=device, weights_only=False)
+        self.brain_net_acc4.load_state_dict(brain_ckpt['model'])
 
-        knee_ckpt = torch.load(knee_model_path, map_location=device, weights_only=False)
-        self.knee_net.load_state_dict(knee_ckpt['model'])
+        brain_ckpt = torch.load(brain_model_acc8_path, map_location=device, weights_only=False)
+        self.brain_net_acc8.load_state_dict(brain_ckpt['model'])
+
+        knee_ckpt = torch.load(knee_model_acc4_path, map_location=device, weights_only=False)
+        self.knee_net_acc4.load_state_dict(knee_ckpt['model'])
+
+        knee_ckpt = torch.load(knee_model_acc8_path, map_location=device, weights_only=False)
+        self.knee_net_acc8.load_state_dict(knee_ckpt['model'])
 
         classifier_ckpt = torch.load(classifier_path, map_location=device, weights_only=False)
         self.classifier.load_state_dict(classifier_ckpt['model'])
@@ -33,11 +43,11 @@ class MoEModel(nn.Module):
         input_img = input_img
         logits = self.classifier(input_img)
         choice = torch.argmax(logits, dim=1)        # 0: brain, 1: knee
-        out = torch.empty_like(masked_kspace)
+        acc = self.acc_classifier(masked_kspace)    # 4 or 8
 
         if choice.item() == 0:
-            out = self.brain_net(masked_kspace, mask)
+            out = self.brain_net_acc4(masked_kspace, mask) if acc == 4 else self.brain_net_acc8(masked_kspace, mask)
         else:
-            out = self.knee_net(masked_kspace, mask)
+            out = self.knee_net_acc4(masked_kspace, mask) if acc == 4 else self.knee_net_acc8(masked_kspace, mask)
 
         return out
